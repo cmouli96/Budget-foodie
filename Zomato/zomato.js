75fd0f590d35c75eaed46c1495a393b2208d49b0
cmouli96 <53935126+cmouli96@users.noreply.github.com>
2020-05-23 19:08:21 +0530

    Add files via upload

--- Zomato/zomato.js
+++ Zomato/zomato.js
@@ -21,7 +21,7 @@ const zomato = {
         })
 
 
-        await page.goto(BASE_URL, { waitUntil: 'networkidle2', timeout: 0 });
+        await page.goto(BASE_URL, { waitUntil: 'networkidle2' });
 
         console.log('Initialization completed..');
     },
@@ -30,7 +30,7 @@ const zomato = {
 
         console.log(`Going to the Product Page.. ( ${link} )`);
 
-        await page.goto(link, { waitUntil: 'networkidle2' ,timeout:0 });
+        await page.goto(link, { waitUntil: 'networkidle2' });
         // let webcontent=await page.content();
         // return webcontent;
 
@@ -44,53 +44,33 @@ const zomato = {
             
            // let restname=document.getElementsByClassName('sc-7kepeu-0 sc-cCbXAZ jVrsaq').innerText;
 
-        //    let din=document.getElementsByClassName("sc-1s0saks-7 fIgnDe");
-        //    let dins=[];
-        //    let  n1=din.length;
-        //    for(var i=0;i<n1;i++)
-        //    {
-        //        dins.push(din[i].innerText);
-        //    }
-        //    //return dins[0].innerText.innerText;
-        //    return{
-        //        dins
-        //    }
+           let din=document.getElementsByClassName("sc-1s0saks-7 fIgnDe");
+           let dins=[];
+           let  n1=din.length;
+           for(var i=0;i<n1;i++)
+           {
+               dins.push(din[i].innerText);
+           }
+           //return dins[0].innerText.innerText;
+           return{
+               dins
+           }
 
 
 
 
             let restname=document.querySelector('h1').innerText;
             //let restnames=[];
-            // let tag1=document.getElementsByClassName("sc-hdPSEv gYwhMC");
-            // let tag=tag1[0].innerText;
-            let tags=document.getElementsByClassName("sc-gleUXh bWIcur");
-            let tag1=tags[0].innerText;
-            let tag2=tag1.split("\n");
-            let tag=tag2[0];
-            let address=tag2[1];
-            let r=document.getElementsByClassName("sc-elNKlv jlVypF");
-            let r1=r[0].innerText;
-            let r2=r1.split("\n");
-            let rating=r2[0];
-            let peopleRated=r2[2];
-
             let rates=[];
             let rate=document.getElementsByClassName('sc-17hyc2s-1 fnhnBd');
+
             let dishname=document.getElementsByClassName("sc-1s0saks-11 cDXzZl");
             let dishes=[];
             let n=dishname.length;
-            let items=[];
             for(var i=0;i<n;i++)
             {
-               let item={};
-               item["itemName"]=dishname[i].innerText;
-               item["description"]="---";
-               item["ztoPrice"]=rate[i].innerText;
-               items.push(item);
-              //  dishes.push(dishname[i].innerText);
+                dishes.push(dishname[i].innerText);
             }
-
-            //   //  sc-RmnOB jZgfXT
 
             //let n=rate.length;
             for(var i=0;i<n;i++)
@@ -99,11 +79,8 @@ const zomato = {
             }
             return{
                 restname,
-                tag,
-                address,
-                rating,
-                peopleRated,
-                items
+                dishes,
+                rates
             }
 
             //<span class="sc-17hyc2s-1 fnhnBd">₹99</span>
